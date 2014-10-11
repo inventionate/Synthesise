677333f604a7e97e13b2a9a1065de0e3c22290c0
Fabian Mundt <f.mundt@inventionate.de>
2014-10-11 11:27:00 +0200

    Add tracking assets for git deploy.

---
+++ public/qunit/tests.js
@@ -0,0 +1,8 @@
+QUnit.test("Just one Test", function( assert ) {
+  assert.equal("just now", "just now");
+});
+
+
+QUnit.test("Just another Test", function( assert ) {
+  assert.equal("Monja", "Monja");
+});
---
+++ resources/views/analytics/partials/visitors.blade.php
@@ -0,0 +1,40 @@
+<section class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">Besucher insgesamt</h3>
+  </div>
+  <div class="panel-body">
+    {{ $liveVisitors }}
+    <canvas id="lChart" height="139px"></canvas>
+  </div>
+</section>
+
+<script>
+
+var lc = $("#lChart").get(0).getContext("2d");
+
+var options = {responsive: true, animationEasing: 'easeOut'};
+
+var data = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "StudentInnen"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "DozentInnen"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "MentorInnen"
+    }
+]
+
+var lChart = new Chart(lc).Pie(data, options);
+
+</script>
