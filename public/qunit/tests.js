af152ebf4ef34761f60385c129d4e370d5cdf9fa
Fabian Mundt <f.mundt@inventionate.de>
2014-10-14 17:00:55 +0200

    Minor JS updates.

--- public/qunit/tests.js
+++
@@ -1,8 +0,0 @@
-QUnit.test("Just one Test", function( assert ) {
-  assert.equal("just now", "just now");
-});
-
-
-QUnit.test("Just another Test", function( assert ) {
-  assert.equal("Monja", "Monja");
-});
