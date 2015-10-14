34cec94c0b8f3548b2591c8c80ee3343a9068a02
inventionate <f.mundt@inventionate.de>
2015-10-15 00:34:01 +0200

    Imprint and Disclamir updates.

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,12 +3,13 @@
         <p>
             <small>
                 © 2012–{{ date("Y",time()) }} Gesamtkonzeption <span class="etpM"><b>e:t:p:M</b></span> Timo Hoyer | Mediengestaltung und Webentwicklung Fabian Mundt<br>
-                <a href="{{ url('impressum') }}">Impressum</a>
+                <a href="{{ url('impressum') }}">Impressum & Datenschutz</a>
                 | <a href="{{ url('demo') }}">Informationen zum <span class="etpM">e:t:p:M</span>-Konzept</a>
                 | <a href="https://etpm-dev.ph-karlsruhe.de/etpm-evaluation/" target="_blank">Evaluationsergebnisse</a>
+                | <a href="https://etpm-dev.ph-karlsruhe.de/hoerspiel/" target="_blank">Hörspiel</a>
             </small>
-            {{-- VERSION --}}
-            <div id="version" class="ui mini basic label"><i class="code icon"></i> {{ env('VERSION') }}</div>
         </p>
+        {{-- VERSION --}}
+        <div id="version" class="ui mini basic label"><i class="code icon"></i> {{ env('VERSION') }}</div>
   </div>
 </footer>
